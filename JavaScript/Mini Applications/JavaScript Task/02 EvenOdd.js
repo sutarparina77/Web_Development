8e604d9ccbf5078de53751daa7ae6cfd589acfad
Parina Sutar <sutarparina77@gmail.com>
2026-08-22 14:21:23 +0530

    Add files via upload

---
+++ JavaScript/Mini Applications/JavaScript Task/02 EvenOdd.js
@@ -0,0 +1,12 @@
+let No = 0;
+
+function Check_Evn_Odd(No)
+{
+    if(No % 2 == 0 )
+    {
+        console.log("Given number" + No + "is Even");
+    }
+    else{
+        console.log("Given number" + No + "is Odd");
+    }
+}
