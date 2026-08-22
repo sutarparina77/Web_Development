c8b056c2c42ed64471c706c7ce332b637f3c4c66
Parina Sutar <sutarparina77@gmail.com>
2026-08-22 14:21:54 +0530

    Add files via upload

---
+++ JavaScript/Mini Applications/JavaScript Task/03 LetterVowelOrConsonant.js
@@ -0,0 +1,11 @@
+let Ch;
+
+function Check_Vowel_Cons(Char){
+    if(Ch == 'A' || Ch == 'E' || Ch == 'I' || Ch == 'O' || Ch == 'U' || Ch == 'a' || Ch == 'e' || Ch == 'i' || Ch == 'o' || Ch == 'u')
+    {
+        console.log("The Character" + Ch + "is Vowel");
+    }
+    else{
+        console.log("The Character" + Ch + "is Consonant");
+    }
+}
