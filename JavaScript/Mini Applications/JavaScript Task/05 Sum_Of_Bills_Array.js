0408388e9faae3b28d548d1dae806dec0b14f249
Parina Sutar <sutarparina77@gmail.com>
2026-08-24 22:47:12 +0530

    Add files via upload

---
+++ JavaScript/Mini Applications/JavaScript Task/05 Sum_Of_Bills_Array.js
@@ -0,0 +1,8 @@
+let Bill = [120, 230, 190, 500, 340, 450, 780, 670, 340, 260], i = 0, B_Sum = 0;
+  
+for(let i = 0; i < 10; i++)
+{ 
+    B_Sum = B_Sum + Bill[i];
+}
+    
+console.log(" Sum of all bills :", B_Sum);
