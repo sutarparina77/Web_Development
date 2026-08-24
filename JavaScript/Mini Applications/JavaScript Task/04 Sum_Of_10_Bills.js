b1408e1b5e3415c91cd8d34fc7479af23ad704ff
Parina Sutar <sutarparina77@gmail.com>
2026-08-24 22:46:49 +0530

    Add files via upload

---
+++ JavaScript/Mini Applications/JavaScript Task/04 Sum_Of_10_Bills.js
@@ -0,0 +1,10 @@
+let Bill = 0, Bill_Sum = 0;
+  
+for(i = 0; i < 10; i++)
+{
+    Bill = Number(prompt("Enter a Number"));
+    Bill_Sum = Bill + Bill_Sum;
+}
+  
+console.log("Sum of 10 bills is = " , Bill_Sum);
+  
