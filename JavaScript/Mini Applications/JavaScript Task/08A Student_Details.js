ac0e6823fe083da086ae5734719b42b228d2da7b
Parina Sutar <sutarparina77@gmail.com>
2026-08-26 17:10:30 +0530

    Add files via upload

---
+++ JavaScript/Mini Applications/JavaScript Task/08A Student_Details.js
@@ -0,0 +1,21 @@
+let Stud_Details = {
+    RNo: 101,
+    Name: "Rohit Sharma",
+    Phy: 89,
+    Chem: 90,
+    Maths: 80,
+    Total: 259,
+    Per: 86.33,
+    Grade: 'A',
+    MNo: 9562145635,
+}
+
+console.log("Roll No :", Stud_Details.RNo);
+console.log("Name :", Stud_Details.Name);
+console.log("Physics Marks :", Stud_Details.Phy);
+console.log("Chemistry Marks :", Stud_Details.Phy);
+console.log("Maths Marks :", Stud_Details.Maths);
+console.log("Total :", Stud_Details.Total);
+console.log("Percentage :", Stud_Details.Per);
+console.log("Grade :", Stud_Details.Grade);
+console.log("Mobile No. :", Stud_Details.MNo);
