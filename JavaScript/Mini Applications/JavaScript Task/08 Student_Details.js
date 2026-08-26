d415e8a4525a032785bef7ce17aff14c459816ae
Parina Sutar <sutarparina77@gmail.com>
2026-08-26 17:10:09 +0530

    Add files via upload

---
+++ JavaScript/Mini Applications/JavaScript Task/08 Student_Details.js
@@ -0,0 +1,21 @@
+let Stud_Details = {
+    RNo: prompt("Enter Student Roll No. : "),
+    Name: prompt("Enter Student Name :"),
+    Phy: prompt("Enter Physics Marks :"),
+    Chem: prompt("Enter Chemistry Marks :"),
+    Maths: prompt("Enter Maths Marks :"),
+    Total: prompt("Enter Total of Marks :"),
+    Per: prompt("Enter Percentage :"),
+    Grade: prompt("Enter Obtained Grade :"),
+    MNo: prompt("Enter Mobile No. :"),
+}
+
+console.log("Roll No :", Stud_Details.RNo);
+console.log("Name :", Stud_Details.Name);
+console.log("Physics Marks :", Stud_Details.Phy);
+console.log("Chemistry Marks :", Stud_Details.Phy);
+console.log("Maths Marks :", Stud_Details.Maths);
+console.log("Total :", Stud_Details.Total);
+console.log("Percentage :", Stud_Details.Per);
+console.log("Grade :", Stud_Details.Grade);
+console.log("Mobile No. :", Stud_Details.MNo);
